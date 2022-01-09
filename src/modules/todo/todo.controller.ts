137a611b3b53af65ef1a07c8fcbbb0ea472866c3
valkln <kalinin.valentyn96@gmail.com>
2022-01-09 12:25:45 +0200

    added nestjsx-crud

--- src/modules/todo/todo.controller.ts
+++ src/modules/todo/todo.controller.ts
@@ -8,52 +8,17 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { CrudController, Crud } from '@nestjsx/crud';
 import { Todo } from './entities/todo.entity';
 import { TodoDto } from './todo.dto';
 import { TodoService } from './todo.service';
 
+@Crud({
+  model: {
+    type: Todo,
+  },
+})
 @Controller('api/todo')
-export class TodoController {
-  constructor(private readonly todoService: TodoService) {}
-  @Get()
-  getTodos() {
-    return this.todoService.getAll();
-  }
-
-  @Get(':id')
-  async getOne(@Param('id') id: string): Promise<Todo> {
-    const todo = await this.todoService.getOne(id);
-    if (todo === undefined) {
-      throw new NotFoundException('Todo Not Found');
-    }
-    return todo;
-  }
-  @Post()
-  createTodo(@Body() todoDto: TodoDto): Promise<Todo> {
-    const todo = new Todo();
-    todo.title = todoDto.title;
-    if (todoDto.isCompleted) {
-      todo.isCompleted = todoDto.isCompleted;
-    }
-    return this.todoService.create(todo);
-  }
-
-  @Put(':id')
-  async updateTodo(
-    @Param('id') id: string,
-    @Body() { title, isCompleted }: TodoDto,
-  ): Promise<Todo> {
-    const todo = await this.todoService.getOne(id);
-    if (todo === undefined) {
-      throw new NotFoundException('Todo Not Found');
-    }
-    todo.title = title;
-    todo.isCompleted = isCompleted;
-    return this.todoService.update(todo);
-  }
-
-  @Delete(':id')
-  deleteTodo(@Param('id') id: string): Promise<void> {
-    return this.todoService.delete(id);
-  }
+export class TodoController implements CrudController<Todo> {
+  constructor(public service: TodoService) {}
 }
--- src/modules/todo/todo.service.ts
+++ src/modules/todo/todo.service.ts
@@ -1,27 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { Todo } from './entities/todo.entity';
 
 @Injectable()
-export class TodoService {
-  constructor(
-    @InjectRepository(Todo)
-    private readonly todoRep: Repository<Todo>,
-  ) {}
-  getAll() {
-    return this.todoRep.find();
-  }
-  getOne(id: string): Promise<Todo> {
-    return this.todoRep.findOne(id);
-  }
-  create(todo: Todo): Promise<Todo> {
-    return this.todoRep.save(todo);
-  }
-  update(todo: Todo) {
-    return this.todoRep.save(todo);
-  }
-  async delete(id: string): Promise<void> {
-    await this.todoRep.delete(id);
+export class TodoService extends TypeOrmCrudService<Todo> {
+  constructor(@InjectRepository(Todo) todoRep) {
+    super(todoRep);
   }
 }
