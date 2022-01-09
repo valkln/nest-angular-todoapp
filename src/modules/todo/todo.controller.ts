3eaa9749fc517068a4c27f29a312caafbbde52c7
valkln <kalinin.valentyn96@gmail.com>
2022-01-09 12:26:24 +0200

    quick fix

--- src/modules/todo/todo.controller.ts
+++ src/modules/todo/todo.controller.ts
@@ -1,16 +1,6 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  NotFoundException,
-  Param,
-  Post,
-  Put,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { CrudController, Crud } from '@nestjsx/crud';
 import { Todo } from './entities/todo.entity';
-import { TodoDto } from './todo.dto';
 import { TodoService } from './todo.service';
 
 @Crud({
